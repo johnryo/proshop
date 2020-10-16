96a5a230d9c7d7b5c898598927c89debb26f343c
John Oggerino <jpogge55@gmail.com>
2020-10-15 21:13:22 -0700

    Start checkout process

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -62,7 +62,7 @@ const ProfileScreen = ({ location, history }) => {
         <Form.Group controlId='name'>
           <Form.Label>Name</Form.Label>
           <Form.Control
-            type='string'
+            type='name'
             placeholder='Enter name'
             value={name}
             onChange={(e) => setName(e.target.value)}
