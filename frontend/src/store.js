11ab9f083c46703ec0a51a18425b94c19e862340
John Oggerino <jpogge55@gmail.com>
2020-10-18 10:27:33 -0700

    Complete checkout process

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -9,7 +9,12 @@ import {
   userDetailsReducer,
   userUpdateProfileReducer,
 } from './reducers/userReducers'
-import { orderCreateReducer } from './reducers/orderReducers'
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+  orderPayReducer,
+  orderListMyOrdersReducer,
+} from './reducers/orderReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -20,6 +25,9 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
+  orderListMy: orderListMyOrdersReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
